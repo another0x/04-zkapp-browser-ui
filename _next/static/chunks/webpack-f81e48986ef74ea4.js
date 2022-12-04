e17d862bed75056773a2206b88baeaee47e87de1
another0x <thejacklimited@gmail.com>
2022-12-04 14:26:20 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-f81e48986ef74ea4.js
+++ _next/static/chunks/webpack-f81e48986ef74ea4.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "03ed19c28124cde1" + ".css";
+/******/ 			return "static/css/" + "08cb0ecbacfd2e51" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
